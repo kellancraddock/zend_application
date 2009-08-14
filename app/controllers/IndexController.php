e17744b1ed8982db2847029543179c3f12a80163
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-14 00:20:56 -0700

    added action helper access  comment

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,7 +4,12 @@
 		public function init() {
 			//$this->session_alert = new Zend_Session_Namespace('');
 			//$this->Model = new Model();
+			
+			//Sets alternative layout
 			//$this->_helper->layout->setLayout('');
+			
+			//Access to helper
+			//$helper = $this->_helper->HelperName;
 		}
 		
 		public function indexAction()
